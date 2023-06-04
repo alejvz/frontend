4366f62cb9845265a81eeb4b3634e9c126b6f9b2
Alejvz <86327633+alejvz@users.noreply.github.com>
2023-06-04 14:13:52 -0500

    M4

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
 
 import '../styles/global.css'
 import { Inter } from 'next/font/google'
-const GA_TRACKING_ID = 'G-13VVY5R2D1'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Analytics from '@/components/Analytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,22 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="es">
       <head>
       <link rel="icon"  href="/assets/vasky.png" />
-      <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
+      <Analytics />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
---
+++ components/Analytics.js
@@ -0,0 +1,20 @@
+import Script from 'next/script'
+
+const Analytics = () => (
+  <Script
+    id="google-analytics"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+    }}
+  />
+)
+
+export default Analytics
