6adc42921d4f061becf0715bf5054764d6d57a01
Alejvz <86327633+alejvz@users.noreply.github.com>
2023-07-19 09:43:54 -0500

    M29

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 
 import '../styles/global.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,9 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-      <link rel="icon"  href="/assets/lulai.png" />
+        <link rel="icon" href="/assets/lulai.png" />
       </head>
-      <body  className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
